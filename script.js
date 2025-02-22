d8b447efb3da9d69891c29a2c4f4ea681475c43f
KnubiEE <148622415+ErikEGH@users.noreply.github.com>
2025-02-22 10:34:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-fetch('manifest.json')
-        .then(response => response.json())  // JSON in ein Objekt umwandeln
-        .then(data => {
-            console.log(data);  // Hier kannst du mit den Daten arbeiten
-        })
-        .catch(error => console.error('Fehler beim Laden der JSON-Datei:', error));
-
 addEventListener('load', function(e) {
-    document.querySelector('#test').innerHTML = 'Zufallsgenerator ';
-    });
+    document.querySelector('#test').innerHTML = 'Zufallsgenerator';
+});
 
+let usedNumbers = [];
+
+function generateRandomNumber() {
+    const anzahlInput = document.getElementById('anzahl');
+    const anzahl = parseInt(anzahlInput.value);
+
+    if (isNaN(anzahl) || anzahl < 1) {
+        alert('Bitte geben Sie eine gültige Anzahl ein.');
+        return;
+    }
+
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '';
+
+    let randomNumber;
+    do {
+        randomNumber = Math.floor(Math.random() * anzahl) + 1;
+    } while (usedNumbers.includes(randomNumber));
+
+    usedNumbers.push(randomNumber);
+
+    const p = document.createElement('p');
+    p.textContent = `Sitzplatz Nr. ${randomNumber}`;
+    outputDiv.appendChild(p);
+}
