9a8f677ed4ed8e296a080f61378eb71f5423bac2
KnubiEE <148622415+ErikEGH@users.noreply.github.com>
2025-02-22 10:28:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+fetch('manifest.json')
+        .then(response => response.json())  // JSON in ein Objekt umwandeln
+        .then(data => {
+            console.log(data);  // Hier kannst du mit den Daten arbeiten
+        })
+        .catch(error => console.error('Fehler beim Laden der JSON-Datei:', error));
+
 addEventListener('load', function(e) {
     document.querySelector('#test').innerHTML = 'Zufallsgenerator ';
     });
